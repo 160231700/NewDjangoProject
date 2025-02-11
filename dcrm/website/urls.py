f5319ddb9ee447feb10f29ffc3e8d37b9893b445
160231700 <144690640+160231700@users.noreply.github.com>
2025-02-11 14:11:40 +0000

    Sigma males

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -1,8 +1,22 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("information", views.information, name="information")
+
+
+    #//// Start of user management////
+    #Shows to a dashboard
+    path("dashboard/", views.dashboard, name="dashboard"),
+    #Links to the sign up page
+    path("sign_up/", views.sign_up, name="sign_up"),
+    #Allows users to login or out without additional urls. Ex: accounts/login/
+    #Below also circumvents the need for additional views
+    path("accounts/", include("django.contrib.auth.urls")),
+
+    #//// End of user management////
+
+    #Shows a page with information about the corporation
+    path("information/", views.information, name="information")
 ]
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+# Remove: from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
+
+# Views for the website/ template
 def index(request):
     return render(request,'website/index.html')
 
 def information(request):
     return render(request,'website/information.html')
+
+def dashboard(request):
+    return render(request, "website/dashboard.html")
+
+
+# Views for the registration/ template
+
+#Creates a new user
+def sign_up(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        #Validates the form filled out by the user
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse("dashboard"))
+    #If the request isn't a post (The form hasn't been sent) it'll send a form to the user
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "registration/sign_up.html", {"form": form})
